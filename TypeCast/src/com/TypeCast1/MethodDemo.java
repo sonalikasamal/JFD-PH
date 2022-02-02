24e1cedfb9a77040fe9be53e31c1da2c0145210e
Sonalika Samal <sonalikasamal123@gmail.com>
2022-02-02 22:15:52 +0530

    First commit

---
+++ TypeCast/src/com/TypeCast1/MethodDemo.java
@@ -0,0 +1,32 @@
+package com.TypeCast1;
+
+
+	public class MethodDemo {
+
+		public int multipynumbers(int a,int b) {
+			int z=a*b;
+			return z;
+		}
+
+		public static void main(String[] args) {
+
+			 MethodDemo b=new  MethodDemo();
+			int ans= b.multipynumbers(10,3);
+			System.out.println("Multipilcation is :"+ans);
+			}
+	}
+
+
+		
+
+
+
+		
+
+
+
+		
+		 
+
+
+
---
+++ TypeCast/src/com/TypeCast1/ParamConst.java
@@ -0,0 +1,31 @@
+package com.TypeCast1;
+
+
+	//parameterized constructor
+		class Std{
+			int id;
+			String name;
+
+			Std(int i,String n)
+			{
+			id=i;
+			name=n;
+			}
+
+			void display() {
+			System.out.println(id+" "+name);
+			}
+		}
+
+		public class ParamConst {
+		public static void main(String[] args) {
+
+			Std std1=new Std(2,"Alex");
+			Std std2=new Std(10,"Annie");
+			std1.display();
+			std2.display();
+				}
+		}
+
+
+
